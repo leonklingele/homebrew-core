gromgit/homebrew-core-aarch64_linux

commit 0e962e1a1102a23ae39bb93b1e076601e4400eee

dep: remove old code.

Formula/dep.rb
@@ -20,11 +20,10 @@ class Dep < Formula
 
   def install
     ENV["GOPATH"] = buildpath
-    arch = MacOS.prefer_64_bit? ? "amd64" : "386"
     (buildpath/"src/github.com/golang/dep").install buildpath.children
     cd "src/github.com/golang/dep" do
       ENV["DEP_BUILD_PLATFORMS"] = "darwin"
-      ENV["DEP_BUILD_ARCHS"] = arch
+      ENV["DEP_BUILD_ARCHS"] = "amd64"
       system "hack/build-all.bash"
       bin.install "release/dep-darwin-#{arch}" => "dep"
       prefix.install_metafiles

leonklingele/homebrew-core

commit 0569f33ff96d8f197490228b8e1ecedd42db93ec

dep: fix use of undefined variable 'arch'
Issue was introduced in 0e962e1a1102a23ae39bb93b1e076601e4400eee.

Formula/dep.rb
@@ -25,7 +25,7 @@ class Dep < Formula
       ENV["DEP_BUILD_PLATFORMS"] = "darwin"
       ENV["DEP_BUILD_ARCHS"] = "amd64"
       system "hack/build-all.bash"
-      bin.install "release/dep-darwin-#{arch}" => "dep"
+      bin.install "release/dep-darwin-amd64" => "dep"
       prefix.install_metafiles
     end
   end